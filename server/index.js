dde14356939d1081434aef05ace1eb8083e0356e
Wei Gao <wei.gao42@gmail.com>
2019-11-02 19:15:27 -0400

    Add connection to AWS RDS DB

--- server/index.js
+++ server/index.js
@@ -6,27 +6,26 @@ const mysql = require('mysql');
 const axios = require('axios');
 const app = express()
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error('Database connection failed: ' + err.stack);
     return;
   }
-
   console.log('Connected to database.');
 });
 
 connection.end();
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
 
 const port = process.env.PORT;
 const apiKey = process.env.API_KEY
